dfbd92722fb8110eefb207fb27ce168ca5a21e6c
ZZ3n <ndsrhkd@naver.com>
2021-06-30 16:06:24 +0900

    first commit

---
+++ src/main/java/site/todadak/Todadak.java
@@ -0,0 +1,11 @@
+package site.todadak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todadak {
+    public static void main(String[] args) {
+        SpringApplication.run(Todadak.class, args);
+    }
+}
